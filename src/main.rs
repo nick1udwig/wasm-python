bffd0be0263662379c6b7687a759d934ff9f4215
hosted-fornet <hosted-fornet@protonmail.com>
2024-01-23 15:36:00 -0800

    scope code for implicit drop; add comments re: ambient auth

--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,45 @@ use wasmtime_wasi::sync::WasiCtxBuilder;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Define the WASI functions globally on the `Config`.
-    let mut config = Config::new();
-    config.async_support(true);
-    let engine = Engine::new(&config)?;
-    let mut linker = Linker::new(&engine);
-    wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
-
-    let dir = cap_std::fs::Dir::open_ambient_dir(
-        ".",
-        cap_std::ambient_authority(),
-    ).unwrap();
-
     let wasi_stdin = ReadPipe::from("import sys; from pprint import pprint as pp; pp(sys.path); pp(sys.platform)");
     let wasi_stdout = WritePipe::new_in_memory();
 
-    // Create a WASI context and put it in a Store; all instances in the store
-    // share this context. `WasiCtxBuilder` provides a number of ways to
-    // configure what the target program will have access to.
-    let wasi = WasiCtxBuilder::new()
-        .preopened_dir(dir, "/")?
-        .stdin(Box::new(wasi_stdin.clone()))
-        .stdout(Box::new(wasi_stdout.clone()))
-        .inherit_args()?
-        .build();
-    let mut store = Store::new(&engine, wasi);
+    {
+        // Define the WASI functions globally on the `Config`.
+        let mut config = Config::new();
+        config.async_support(true);
+        let engine = Engine::new(&config)?;
+        let mut linker = Linker::new(&engine);
+        wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
 
-    // Instantiate our module with the imports we've created, and run it.
-    let module = Module::from_file(&engine, "python-3.12.0.wasm")?;
-    linker.module_async(&mut store, "", &module).await?;
-    linker
-        .get_default(&mut store, "")?
-        .typed::<(), ()>(&store)?
-        .call_async(&mut store, ()).await?;
+        // uncomment to bring in non-stdlib libs (except those containing C code)
+        // let dir = cap_std::fs::Dir::open_ambient_dir(
+        //     "venv",
+        //     cap_std::ambient_authority(),
+        // ).unwrap();
 
-    drop(store);
+        // Create a WASI context and put it in a Store; all instances in the store
+        // share this context. `WasiCtxBuilder` provides a number of ways to
+        // configure what the target program will have access to.
+        let wasi = WasiCtxBuilder::new()
+            // uncomment to bring in non-stdlib libs (except those containing C code)
+            // .preopened_dir(dir, "/venv")?
+            // .env("PYTHONPATH", "/venv/lib/python3.12/site-packages")?
+            .stdin(Box::new(wasi_stdin.clone()))
+            .stdout(Box::new(wasi_stdout.clone()))
+            .inherit_args()?
+            .build();
+        let mut store = Store::new(&engine, wasi);
+
+        // Instantiate our module with the imports we've created, and run it.
+        let module = Module::from_file(&engine, "python-3.12.0.wasm")?;
+        linker.module_async(&mut store, "", &module).await?;
+        linker
+            .get_default(&mut store, "")?
+            .typed::<(), ()>(&store)?
+            .call_async(&mut store, ()).await?;
+    }
+
     let contents: Vec<u8> = wasi_stdout
         .try_into_inner()
         .expect("sole remaining reference to WritePipe")
